4c163a2fab95c3599ff5692627410bb9954f8624
Vaibhav Gawad <gawadvaibhavv@gmail.com>
2024-01-17 19:44:50 +0530

    Cors error fix

---
+++ backend/src/pages/api/jobs/index.ts
@@ -0,0 +1,52 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { prisma } from "../../../utils/db";
+
+const getAllJobs = async (req, res) => {
+  try {
+      const jobs = await prisma.job.findMany({
+      });
+
+    
+    res.status(200).json({ jobs });
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+const createJob = async (req, res) => {
+  try {
+    const { title, description, location, salary } = req.body;
+    const job = await prisma.job.create({
+      data: {
+        title,
+        description,
+        location,
+        salary,
+      },
+    });
+    res.status(200).json({ job });
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error" });
+  }
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const method = req.method;
+  switch (method) {
+    case "GET":
+      getAllJobs(req, res);
+      break;
+    case "POST":
+      break;
+    case "PATCH":
+      break;
+    case "DELETE":
+      break;
+    default:
+      res.status(500).json({ message: "Method not allowed" });
+      break;
+  }
+}
