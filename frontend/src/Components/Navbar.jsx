4b26ee79ec6e26cf9fa1f7ba3391eaaac6a14854
Vaibhav Gawad <gawadvaibhavv@gmail.com>
2024-01-19 10:17:27 +0530

    adasdfadfasdfasdf

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -5,6 +5,7 @@ import { auth } from "../firebaseConfig";
 import TTS from "./TTS";
 import "./bgcolor.css";
 import logo from "../assets/reservedin.png";
+import { toast } from "react-toastify";
 import { applyColorblindFilter } from "../utils/colors";
 
 function Navbar() {
@@ -25,117 +26,171 @@ function Navbar() {
   }, [colorBlind]);
 
   return (
-    <header className="text-gray-600 body-font">
-      <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-        <a
-          aria-label="Click here to go to home page"
-          className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0"
-          onClick={() => {
-            navigate("/home");
-          }}
-        >
-          <img
-            className="w-44"
-            src={logo}
-            alt="ReservedIn"
-            border="0"
-            width="50"
-            height="50"
-          />
+    <div>
+      <header className="text-gray-600 body-font">
+        <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+          <a
+            aria-label="Click here to go to home page"
+            className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0"
+            onClick={() => {
+              navigate("/home");
+            }}
+          >
+            <img
+              className="w-44"
+              src={logo}
+              alt="ReservedIn"
+              border="0"
+              width="50"
+              height="50"
+            />
 
-          {/* <span className="ml-3 text-xl">ReservedIn</span> */}
-        </a>
-        <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400 flex flex-wrap items-center text-base justify-center">
-          <br />
+            {/* <span className="ml-3 text-xl">ReservedIn</span> */}
+          </a>
+          <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400 flex flex-wrap items-center text-base justify-center">
+            <br />
 
-          <button
-            className="mr-5" // Added mb-2 for bottom margin
-            aria-label="Click here to go to Job portal"
-            onClick={() => navigate("/jobs")}
-          >
-            Jobs
-          </button>
+            <button
+              className="mr-5" // Added mb-2 for bottom margin
+              aria-label="Click here to go to Job portal"
+              onClick={() => navigate("/jobs")}
+            >
+              Jobs
+            </button>
 
-          <br />
-          <button
-            className="mr-5"
-            aria-label="Click here to go to resume maker"
-            onClick={() => navigate("/resume")}
-          >
-            Resume Builder
-          </button>
-          <br />
-          <button
-            className="mr-5"
-            aria-label="This is blogpage"
-            onClick={() => navigate("/blogs")}
-          >
-            Blogs
-          </button>
-          <br />
-          <button
-            className="mr-5"
-            aria-label="This is for courses"
-            onClick={() => navigate("/coursepage")}
-          >
-            Resourses
-          </button>
-          <br />
-          <button
-            className="mr-5"
-            aria-label="This is mentorship page"
-            onClick={() => navigate("/mentorship")}
-          >
-            Mentorships
-          </button>
-          <br />
-          <button
-            className="mr-5"
-            aria-label="This is grievance page"
-            onClick={() => navigate("/grievance")}
-          >
-            Grievance
-          </button>
-          <button
-            className="mr-5"
-            aria-label="This is about page"
-            onClick={() => navigate("/about")}
-          >
-            About Us
-          </button>
+            <br />
+            <button
+              className="mr-5"
+              aria-label="Click here to go to resume maker"
+              onClick={() => navigate("/resume")}
+            >
+              Resume Builder
+            </button>
+            <br />
+            <button
+              className="mr-5"
+              aria-label="This is blogpage"
+              onClick={() => navigate("/blogs")}
+            >
+              Blogs
+            </button>
+            <br />
+            <button
+              className="mr-5"
+              aria-label="This is for courses"
+              onClick={() => navigate("/coursepage")}
+            >
+              Resourses
+            </button>
+            <br />
+            <button
+              className="mr-5"
+              aria-label="This is mentorship page"
+              onClick={() => navigate("/mentorship")}
+            >
+              Mentorships
+            </button>
+            <br />
+            <button
+              className="mr-5"
+              aria-label="This is grievance page"
+              onClick={() => navigate("/grievance")}
+            >
+              Grievance
+            </button>
+            <button
+              className="mr-5"
+              aria-label="This is about page"
+              onClick={() => navigate("/about")}
+            >
+              About Us
+            </button>
 
-          <br />
+            <br />
 
-          {/* <a className="mr-5 hover:text-gray-900">Second Link</a> */}
-          {/* <a className="mr-5 hover:text-gray-900">Third Link</a> */}
-          {/* <a className="mr-5 hover:text-gray-900">Fourth Link</a> */}
-        </nav>
-        <button
-          aria-label="to logout press logout"
-          onClick={
-            localStorage.getItem("userId")
-              ? () => {
-                  localStorage.removeItem("userId");
-                  window.location.href = "/";
-                }
-              : () => {
-                  window.location.href = "/";
-                }
-          }
-          className="inline-flex items-center bg-blue-500 border-0 py-1 px-3 focus:outline-none hover:bg-blue-500 rounded text-base text-white mt-4 md:mt-0"
-        >
-          {localStorage.getItem("userId") ? "Logout" : "Login"}
-          <svg
-            fill="none"
-            stroke="currentColor"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            className="w-4 h-4 ml-1"
-            viewBox="0 0 24 24"
+            {/* <a className="mr-5 hover:text-gray-900">Second Link</a> */}
+            {/* <a className="mr-5 hover:text-gray-900">Third Link</a> */}
+            {/* <a className="mr-5 hover:text-gray-900">Fourth Link</a> */}
+          </nav>
+          {localStorage.getItem("disabilityType") !== "visually-impaired" && (
+            <div className="flex ">
+              <div id="Colorblind-Picker">{/* ... (Colorblindly HTML) */}</div>
+
+              <select
+                onChange={(e) => {
+                  applyColorblindFilter(e.target.value);
+                }}
+                data-choose-theme
+                className="mr-2"
+              >
+                <option value="normal">Normal</option>
+                <option value="achromatomaly">Achromatomaly</option>
+                <option value="deuteranomaly">Deuteranomaly</option>
+                <option value="deuteranopia">Deuteranopia</option>
+                <option value="protanomaly">Protanomaly</option>
+                <option value="protanopia">Protanopia</option>
+                <option value="tritanomaly">Tritanomaly</option>
+                <option value="tritanopia">Tritanopia</option>
+              </select>
+            </div>
+          )}
+
+          <TTS />
+          <button
+            aria-label="to logout press logout"
+            onClick={
+              localStorage.getItem("userId")
+                ? () => {
+                    localStorage.removeItem("userId");
+                    window.location.href = "/";
+                  }
+                : () => {
+                    window.location.href = "/";
+                  }
+            }
+            className="inline-flex items-center bg-blue-500 border-0 py-1 px-3 focus:outline-none hover:bg-blue-500 rounded text-base text-white mt-4 md:mt-0"
           >
-            <path d="M5 12h14M12 5l7 7-7 7"></path>
-          </svg>
+            {localStorage.getItem("userId") ? "Logout" : "Login"}
+            <svg
+              fill="none"
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              className="w-4 h-4 ml-1"
+              viewBox="0 0 24 24"
+            >
+              <path d="M5 12h14M12 5l7 7-7 7"></path>
+            </svg>
+          </button>
+        </div>
+      </header>
+      <div className="bg-gray-100 p-4">
+        <button 
+        onClick={() => {
+           fetch("http://127.0.0.1:8000/start/eyemoment").then((res) => {
+            toast.success("Eye Movement Tracking Started");
+
+          })
+          .catch((err) => {
+            toast.error("Error in starting Eye Movement Tracking");
+          });
+        }}
+         className="bg-gray-100 hover:bg-gray-200 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center">
+          Eye Movement Tracking Start
+        </button>
+        <button 
+        onClick={() => {
+          fetch("http://127.0.0.1:8000/stop/eyemoment").then((res) => {
+           toast.success("Eye Movement Tracking Stoped!");
+
+         })
+         .catch((err) => {
+           toast.error("Error in stopping Eye Movement Tracking");
+         });
+       }}
+        className="bg-gray-100 hover:bg-gray-200 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center">
+          Eye Movement Tracking Stop
         </button>
 
         <TTS />
@@ -155,7 +210,7 @@ function Navbar() {
           />
         </button>
       </div>
-    </header>
+    </div>
   );
 }
 
